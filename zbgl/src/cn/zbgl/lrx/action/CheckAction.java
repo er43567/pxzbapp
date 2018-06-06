a79b5cc46ad5725aec259d3ef6d2e09850faf8a6
Liangming985 <34000463+er43567@users.noreply.github.com>
2018-06-06 21:53:03 +0800

    Revert "fhzbID修改"
    This reverts commit 9b400c6be494aaf7b6e590ad0d85c07ace084966.

--- zbgl/src/cn/zbgl/lrx/action/CheckAction.java
+++ zbgl/src/cn/zbgl/lrx/action/CheckAction.java
@@ -1,35 +1,15 @@
-package cn.zbgl.lm.action;
+package cn.zbgl.lrx.action;
 
 import org.apache.struts2.convention.annotation.Namespace;
 import org.apache.struts2.convention.annotation.ParentPackage;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 
-import cn.zbgl.bean.Fhzb;
-import cn.zbgl.common.CommonAction;
-
 @Namespace("/")
 @ParentPackage("json-default")//非json时，则为"struts-default"
 
 @Controller("checkAction")
 @Scope("prototype")
-public class CheckAction extends CommonAction {
-	
-	public Fhzb fhzb = new Fhzb();
-	
-	public Fhzb getFhzb() {
-		return fhzb;
-	}
-	
-	public String submitFhzb() {
-		System.out.println(fhzb);
-		
-		return aa;
-	}
-	
-	@Override
-	public String getResult() {
-		return result;
-	}
-	
+public class CheckAction {
+
 }
