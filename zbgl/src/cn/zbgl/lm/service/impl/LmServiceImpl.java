00125825e675bbae80ddff89dcea4f42054a52c5
Liangming985 <34000463+er43567@users.noreply.github.com>
2018-06-06 21:55:43 +0800

    Revert "Revert "fhzbID修改""
    This reverts commit a79b5cc46ad5725aec259d3ef6d2e09850faf8a6.

--- zbgl/src/cn/zbgl/lm/service/impl/LmServiceImpl.java
+++ zbgl/src/cn/zbgl/lm/service/impl/LmServiceImpl.java
@@ -1,12 +1,14 @@
 package cn.zbgl.lm.service.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import cn.zbgl.adapters.DaoAdapter;
+import cn.zbgl.bean.Fhzb;
 import cn.zbgl.lm.dao.LmDao;
 import cn.zbgl.lm.service.LmService;
-import cn.zbgl.bean.Zhuangbei;
 
 
 public class LmServiceImpl implements LmService {
@@ -20,6 +22,7 @@ public class LmServiceImpl implements LmService {
 	public void CheckService() {
 		System.out.println("LmService, lmDao = " + dao);
 		dao.CheckDao();
+		
 
 
 	}
