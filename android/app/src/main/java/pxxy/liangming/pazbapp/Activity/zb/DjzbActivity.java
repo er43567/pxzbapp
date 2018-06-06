a8f2432b2c156264151455e91598f642bc853bfe
lrx <601740275@qq.com>
2018-06-06 21:57:09 +0800

    1

--- android/app/src/main/java/pxxy/liangming/pazbapp/Activity/zb/DjzbActivity.java
+++ android/app/src/main/java/pxxy/liangming/pazbapp/Activity/zb/DjzbActivity.java
@@ -19,21 +19,18 @@ public class DjzbActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.xiaxc_danjingzb);
-
-        //初始化组建
-        initWidgets();
-
-    }
-
-    private void initWidgets() {
-        Button commit = findViewById(R.id.confirm);
+        Button commit;
+        commit=findViewById(R.id.confirm);
         commit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getApplication(),"数据保存成功",Toast.LENGTH_SHORT).show();
+                
             }
         });
+
     }
+
 
 
 }
